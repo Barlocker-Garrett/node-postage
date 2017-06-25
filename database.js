0a16cfd973da3b16a4c553748ee293691729cf8b
Garrett <garbar94@gmail.com>
2017-06-24 22:49:43 -0600

    changed to heroku pg config?

--- database.js
+++ database.js
@@ -1,15 +1,14 @@
 var pg = require('pg')
-var PGUSER = 'postgres'
-var PGDATABASE = 'sequence'
+pg.defaults.ssl = true;
 
-var config = {
+/*var config = {
   user: PGUSER, // name of the user account
   database: PGDATABASE, // name of the database
   password: 'root',
   max: 50, // max number of clients in the pool
   idleTimeoutMillis: 30000 
-}
+}*/
 
-var pool = new pg.Pool(config);
+var pool = new pg.Pool();
 
 module.exports = pool;
