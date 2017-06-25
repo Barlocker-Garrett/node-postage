b158577f2976a0cad4e97f4e47623276c3d8e27b
Garrett <garbar94@gmail.com>
2017-06-24 23:27:42 -0600

    added creds

--- database.js
+++ database.js
@@ -1,14 +1,15 @@
 var pg = require('pg')
 pg.defaults.ssl = true;
 
-/*var config = {
-  user: PGUSER, // name of the user account
-  database: PGDATABASE, // name of the database
-  password: 'root',
+var config = {
+  url: 'ec2-54-163-254-76.compute-1.amazonaws.com',
+  user: 'dmevjejangdqdf', // name of the user account
+  database: 'detl5lhln3583p', // name of the database
+  password: '075885152140cafe91c44c910183efcdd5dc8bf1b43261ee57064b23adbd57ef',
   max: 50, // max number of clients in the pool
   idleTimeoutMillis: 30000 
-}*/
+}
 
-var pool = new pg.Pool();
+var pool = new pg.Pool(config);
 
 module.exports = pool;
